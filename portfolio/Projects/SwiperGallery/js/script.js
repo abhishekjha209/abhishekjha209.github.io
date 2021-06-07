8d2c08655b41ff46f56fb9849971f2c27ebf382b
Abhishek Jha <abhs.jha@gmail.com>
2021-06-07 16:00:22 +0530

    Update script.js

--- portfolio/Projects/SwiperGallery/js/script.js
+++ portfolio/Projects/SwiperGallery/js/script.js
@@ -5,5 +5,35 @@ new Swiper('.swiper-container', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    autoplay: {
+      delay: 4000,
+    },
+    parallax:true, 
 
   });
+
+
+//navigation popmotion (spring)
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.brand');//our-item
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
+});
