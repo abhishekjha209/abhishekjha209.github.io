de24201d1b889914231bad3b31db8d42b11c45ea
Abhishek Jha <abhs.jha@gmail.com>
2021-07-05 23:09:32 +0530

    Updated Styling and Script
    Added auto-play feature in Script for swiper-gallary (home page)

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,9 +1,12 @@
 new Swiper('.swiper-container', {
-    speed:400,
-    spaceBetween: 100,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-  });
+  speed:400,
+  spaceBetween: 100,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+  },
+  parallax:true, 
+});
